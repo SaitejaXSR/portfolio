85697735c125f8287b0da785e5902f9ba20ff2a2
Sai teja chary <saitejachary1411@gmail.com>
2023-04-23 15:09:16 +0530

    Add columns for app description, images, links

--- main.py
+++ main.py
@@ -11,8 +11,12 @@ with col2:
     st.title('Sai Teja Chary')
     intro = """
         Hi, I am Sai Teja! I am a Python programmer. I graduated in 2020 with a Bachelor of Technology 
-        in Electrical and Electronics Engineering. Currently working as Senior Software Engineer at Capgemini, 
-        providing services as an Automation Tester for Morgan Stanley.
+        in Electrical and Electronics Engineering from Jawaharlal Nehru Technological University. 
+        Currently working as Senior Software Engineer at Capgemini, providing services as an Automation Tester for Morgan Stanley.
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure
+         dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+         Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
         """
     st.info(intro)
 
@@ -21,14 +25,28 @@ Below You can find some of the apps I have built in Python. Feel free to contact
 """
 st.write(content)
 
-col3, col4 = st.columns(2)
+col3, empty_col, col4 = st.columns([1.5, 0.5, 1.5])
 
 df = pandas.read_csv("data.csv")
 
 with col3:
     for index, row in df[:10].iterrows():
         st.header(row['title'])
+        st.write(row['description'])
+        st.image('images/' + row['image'])
+        source_url, app_url = st.columns(2)
+        with source_url:
+            st.write(f"[Source Code]({row['sourceurl']})")
+        with app_url:
+            st.write(f"[Try App]({row['appurl']})")
 
 with col4:
     for index, row in df[10:].iterrows():
         st.header(row['title'])
+        st.write(row['description'])
+        st.image('images/' + row['image'])
+        source_url, app_url = st.columns(2)
+        with source_url:
+            st.write(f"[Source Code]({row['sourceurl']})")
+        with app_url:
+            st.write(f"[Try App]({row['appurl']})")
