3bf84229f142947e2e61d66c49521880211678e8
Sai teja chary <saitejachary1411@gmail.com>
2023-04-23 11:53:51 +0530

    Create columns, image and description

---
+++ main.py
@@ -0,0 +1,16 @@
+import streamlit as st
+
+st.set_page_config(layout='wide')
+col1, col2 = st.columns(2)
+
+with col1:
+    st.image('images/photo.png')
+
+with col2:
+    st.title('Sai Teja Chary')
+    intro = """
+        Hi, I am Sai Teja! I am a Python programmer. I graduated in 2020 with a Bachelor of Technology 
+        in Electrical and Electronics Engineering. Currently working as Senior Software Engineer at Capgemini, 
+        providing services as an Automation Tester for Morgan Stanley.
+        """
+    st.info(intro)
